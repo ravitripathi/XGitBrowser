b3cfd99559db5b39d6302abea8f8b5eec20a844d
Ravi Tripathi <ravitripathi1996@gmail.com>
2020-10-26 01:31:21 +0530

    Refactoring

--- lib/FollowingList.dart
+++ lib/FollowingList.dart
@@ -29,28 +29,7 @@ class _FollowingListState extends State<FollowingList> {
       itemBuilder: (context, index) {
         final item = followingList[index];
 
-        return SizedBox(
-          width: 150.0,
-          height: 150.0,
-          child: Card(
-              child: Column(
-            children: [
-              Container(
-                margin: EdgeInsets.all(10.0),
-                width: 100,
-                height: 100,
-                decoration: BoxDecoration(
-                  border: Border.all(color: Colors.white, width: 4.0),
-                  boxShadow: [BoxShadow(blurRadius: 4.0)],
-                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
-                  image: DecorationImage(
-                      image: NetworkImage(item.avatar_url), fit: BoxFit.fill),
-                ),
-              ),
-              Text(item.login)
-            ],
-          )),
-        );
+        return FollowingListItem(item: item);
       },
     ));
   }
@@ -74,5 +53,44 @@ class _FollowingListState extends State<FollowingList> {
     });
 
     // Connector().get()
+  }
+}
+
+class FollowingListItem extends StatelessWidget {
+  const FollowingListItem({
+    Key key,
+    @required this.item,
+  }) : super(key: key);
+
+  final FollowingListModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    return GestureDetector(
+      onTap: () {},
+      child: SizedBox(
+        width: 150.0,
+        height: 150.0,
+        child: Card(
+          child: Column(
+            children: [
+              Container(
+                margin: EdgeInsets.all(10.0),
+                width: 100,
+                height: 100,
+                decoration: BoxDecoration(
+                  border: Border.all(color: Colors.white, width: 4.0),
+                  boxShadow: [BoxShadow(blurRadius: 4.0)],
+                  borderRadius: BorderRadius.all(Radius.circular(50.0)),
+                  image: DecorationImage(
+                      image: NetworkImage(item.avatar_url), fit: BoxFit.fill),
+                ),
+              ),
+              Text(item.login),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
--- lib/RepoList.dart
+++ lib/RepoList.dart
@@ -3,6 +3,8 @@ import 'dart:convert';
 import 'package:http/http.dart' as http;
 import 'Url.dart';
 import 'RepoListModel.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:webview_flutter/webview_flutter.dart';
 
 class RepoList extends StatefulWidget {
   final String username;
@@ -27,21 +29,31 @@ class _RepoListState extends State<RepoList> {
       itemBuilder: (context, index) {
         final item = repoList[index];
 
-        return SizedBox(
-          // height: 100.0,
-          child: Expanded(
-            child: Card(
-                child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(item.name),
-                Text(item.description ?? "No Description"),
-              ],
-            )),
-          ),
-        );
+        return GestureDetector(
+            onTap: () {
+              openLink(item.html_url);
+            },
+            child: RepoItemWidget(item: item));
       },
     ));
+  }
+
+  void openLink(String urlString) {
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => Scaffold(
+          appBar: AppBar(
+            title: Text("XGitBrowser"),
+          ),
+          body: SafeArea(
+            child: WebView(
+              initialUrl: urlString,
+            ),
+          ),
+        ),
+      ),
+    );
   }
 
   @override
@@ -60,5 +72,44 @@ class _RepoListState extends State<RepoList> {
       print(res.body);
       loading = false;
     });
+  }
+}
+
+class RepoItemWidget extends StatelessWidget {
+  const RepoItemWidget({
+    Key key,
+    @required this.item,
+  }) : super(key: key);
+
+  final RepoListModel item;
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: Padding(
+            padding: const EdgeInsets.all(4.0),
+            child: Card(
+                shadowColor: Colors.black87,
+                child: Padding(
+                  padding: const EdgeInsets.all(8.0),
+                  child: Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
+                    children: [
+                      Text(item.name,
+                          style: GoogleFonts.roboto(
+                            fontSize: 18.0,
+                            fontWeight: FontWeight.bold,
+                          )),
+                      Text(item.description ?? "No Description"),
+                      Text("Language: ${item.language}"),
+                    ],
+                  ),
+                )),
+          ),
+        )
+      ],
+    );
   }
 }
